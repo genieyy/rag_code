09d7fc01a74e118b5ef4d36fe6f470fa6c1c1118
genie <85426091+genieyy@users.noreply.github.com>
2024-10-19 14:12:57 +0800

    Update llm_response.py

--- deal_data/llm_response.py
+++ deal_data/llm_response.py
@@ -5,7 +5,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
 def get_response_deepseek(new_massages):
-    client = OpenAI(api_key="sk-0c621ec78f624e818dcd110f5a12993d", base_url="https://api.deepseek.com/beta")
+    client = OpenAI(api_key="###########", base_url="https://api.deepseek.com/beta")
 
     response = client.chat.completions.create(
         model="deepseek-coder",
@@ -18,7 +18,7 @@ def get_response_deepseek(new_massages):
     return response.choices[0].message.content
 
 def get_response_gpt4o(new_massages):
-    client = OpenAI(api_key="sk-RNKvKjo6bwNpGC79E3D904Df5f32464fA08b251235782aFd",base_url="http://29qg.com/v1/")
+    client = OpenAI(api_key="###########",base_url="############")
     response = client.chat.completions.create(
         model="gpt-4o-2024-08-06",
         messages=[
